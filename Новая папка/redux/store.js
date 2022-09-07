967705df3b2b9ce3e88677e4cd43ee71bdfa282b
Sergei Maznoi <s.maznoi888@gmail.com>
2022-09-07 18:05:41 +0300

    change project

--- Новая папка/redux/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { contactsReducer } from "./rootReducer";
-
-export const store = configureStore({
-    reducer: {
-        contacts: contactsReducer
-    }
-});
